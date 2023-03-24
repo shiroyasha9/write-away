9d16dbae66f48f08bb1659ddd7f12407f763ca8e
Mubin Ansari <mubinansari910@gmail.com>
2023-03-24 08:21:31 +0530

    add hover effect on NoteCard

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function NoteCard({ note }) {
   return (
     <Link
-      className="flex h-full w-full flex-1 cursor-pointer flex-col justify-center rounded-lg bg-lemon/75 px-12 py-4"
+      className="flex h-full w-full flex-1 cursor-pointer flex-col justify-center rounded-lg bg-lemon/75 px-12 py-4 hover:bg-lemon/90"
       href={`/notes/${note.id}`}
     >
       <h2 className="mb-2 text-xl font-bold text-black">{note.title}</h2>
