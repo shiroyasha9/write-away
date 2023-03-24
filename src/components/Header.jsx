990a6b4edd3459f05e587466cb5d7f5f27adc925
Hitesh Meta <meta.hitesh85@gmail.com>
2023-03-24 23:07:34 +0530

    Remove sticky from header and create grid for note cards

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Button from './Button';
 export default function Header() {
   const router = useRouter();
   return (
-    <div className="sticky top-5 flex w-full justify-between">
+    <div className="flex w-full justify-between">
       <h1 className="text-3xl font-semibold">Write Away</h1>
       <Button onClick={() => router.push('/add')}>Add Note</Button>
     </div>
--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -6,8 +6,8 @@ export default function NoteCard({ note }) {
       className="flex h-full w-full flex-1 cursor-pointer flex-col justify-center rounded-lg bg-lemon/75 px-12 py-4 hover:bg-lemon/90"
       href={`/notes/${note.id}`}
     >
-      <h2 className="mb-2 text-xl font-bold text-black">{note.title}</h2>
-      <p className="text-gray-700">{note.description}</p>
+      <h2 className="mb-2 text-xl font-bold text-black truncate">{note.title}</h2>
+      <p className="text-gray-700 truncate">{note.description}</p>
     </Link>
   );
 }
