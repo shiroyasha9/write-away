c9d630ceb47f5103ad6d34f16d5c816997ee3e9e
Mubin Ansari <mubinansari910@gmail.com>
2023-03-24 06:06:06 +0530

    add prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  endOfLine: 'auto'
+};
+
+module.exports = config;
