605095163fb576530dcca79f0c1a04f4ca451e07
Mubin Ansari <mubinansari910@gmail.com>
2023-03-24 08:19:39 +0530

    use next/link instead of next/router for NoteCard

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,16 +1,13 @@
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 export default function NoteCard({ note }) {
-  const router = useRouter();
   return (
-    <div
+    <Link
       className="flex h-full w-full flex-1 cursor-pointer flex-col justify-center rounded-lg bg-lemon/75 px-12 py-4"
-      onClick={() => {
-        router.push(`/notes/${note.id}`);
-      }}
+      href={`/notes/${note.id}`}
     >
       <h2 className="mb-2 text-xl font-bold text-black">{note.title}</h2>
       <p className="text-gray-700">{note.description}</p>
-    </div>
+    </Link>
   );
 }
