bfa3c6af66ac6d83bb19278c7c9729d98d074f55
Mubin Ansari <mubinansari910@gmail.com>
2023-03-24 07:10:37 +0530

    get other props and classname for Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,9 @@
-export default function Button({ children }) {
+export default function Button({ children, className, ...props }) {
   return (
-    <button className="rounded bg-lemon py-2 px-4 font-bold text-black hover:bg-lemon-dark">
+    <button
+      className={`rounded bg-lemon py-2 px-4 font-bold text-black hover:bg-lemon-dark ${className}`}
+      {...props}
+    >
       {children}
     </button>
   );
